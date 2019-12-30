9f96ab49aeb3ad7c1b40c72d10f75c7df1cf019f
shauangel <37230941+shauangel@users.noreply.github.com>
2019-12-30 16:42:38 +0800

    final
    final

---
+++ script.js
@@ -0,0 +1,78 @@
+var t_1895 = "<h1>1895s 排球源起：</h1>&nbsp;&nbsp;&nbsp;&nbsp;由美國麻省霍利奧克基督教青年會幹事<mark class='yellow'>威廉.摩根</mark>發明。本以<mark class='yellow'>網球場</mark>為場地，人們分別站在兩側托球，人數、擊球次數皆不限制。網子高度1.98米，名稱為<mark class='yellow'>'Mintonette'(小網子)</mark>。<img src='events/William_G._Morgan.jpg' height='120px' style='float: right;'>";
+$('#1895_t').append(t_1895);
+
+var t_1896 = "<h1>1896s 第一場公開賽：</h1>&nbsp;&nbsp;&nbsp;&nbsp;在麻省斯普林菲爾德的<mark class='yellow'>國際YMCA訓練學校(Springfield College)</mark>，舉辦了第一場公開賽。經過比賽，霍爾斯泰德教授提議改為<mark class='yellow'>Volleyball(空中擊球)</mark>。<img src='events/Springfield.jpg' height='120px' style='float: right;'>";
+$('#1896_t').append(t_1896);
+
+var t_1900 = "<h1>1900s 專用球：</h1>&nbsp;&nbsp;&nbsp;&nbsp;正式用球<mark class='yellow'>圓周為25~27英寸(約63.5~68.8cm)</mark>，<mark class='yellow'>重9~12盎司(約255~346g)</mark>。現代國際比賽用球的材料和製作工藝有很大改變，但球的規格和以前差不多。<img src='events/volleyball.jpg' height='120px' style='float: right;'>";
+$('#1900_t').append(t_1900);
+
+var t_1916 = "<h1>1916s 舉球、攻擊的進攻模式：</h1>&nbsp;&nbsp;&nbsp;&nbsp;舉球攻擊的進攻模式源自<mark class='yellow'>菲律賓</mark>，並且沿用至現今。<br><img src='events/spike.jpg' height='120px' style='float: right;' title='姊姊很帥'>";
+$('#1916_t').append(t_1916);
+
+var t_1917 = "<h1>1917s 計分制、得分制的變動：</h1>&nbsp;&nbsp;&nbsp;&nbsp;一開始，排球採<mark class='yellow'>21分制</mark>(同網球)，1917年時，改為<mark class='yellow'>15分發球得分制</mark>。";
+$('#1917_t').append(t_1917);
+
+var t_1920 = "<h1>1920s 新的場上規則：</h1>1.一邊<mark class='yellow'>三次擊球</mark>限制<br>2.<mark class='yellow'>後排攻擊</mark>不可踩三米線<br>3.一人<mark class='yellow'>不可二次擊球</mark><br>4.壓線得分<br>5.發球不可越界，且必須以<mark class='yellow'>撞擊</mark>發球";
+$('#1920_t').append(t_1920);
+
+var t_1922 = "<h1>1922s 首屆YMCA美國錦標賽：</h1>&nbsp;&nbsp;&nbsp;&nbsp;<mark class='yellow'>第一屆YMCA美國錦標賽</mark>在<mark class='yellow'>紐約布鲁克林</mark>開打，一共有來自11個不同洲的27支隊伍參賽。<br><img src='events/ymca_1922.jpg' height='120px' style='float: right;'>";
+$('#1922_t').append(t_1922);
+
+var t_1928 = "<h1>1928s 全美排球協會：</h1>&nbsp;&nbsp;&nbsp;&nbsp;原稱United States Volleyball Association (USVBA)，現今為<mark class='yellow'>USA Volleyball</mark>。由<mark class='yellow'>YMCA(基督教青年會)美國分會</mark>成立，屬於非營利組織，現在為美國排球項目的管理機構。組織總部設立於<mark class='yellow'>美國科羅拉多州科羅拉多泉</mark>。";
+$('#1928_t').append(t_1928);
+
+var t_1947 = "<h1>1947s FIVB：</h1>&nbsp;&nbsp;&nbsp;&nbsp;國際排球總會(Fédération Internationale de Volleyball)，成立於<mark class='yellow'>法國巴黎</mark>，首任主席為法國籍保羅·利鮑德。負責<mark class='yellow'>管理排球類世界級賽事、制定規則</mark>，目前總部位於<mark class='yellow'>瑞士洛桑</mark>，現任主席為巴西籍阿里-格拉薩。";
+$('#1947_t').append(t_1947);
+
+var t_1949 = "<h1>1949s 男子世界錦標賽：</h1>&nbsp;&nbsp;&nbsp;&nbsp;國際排聯舉辦的男子國際賽事，每四年舉辦一次，首屆舉辦於<mark class='yellow'>捷克斯洛伐克布拉格</mark>。此時參賽隊伍仍限於歐洲國家，參賽隊伍數僅10隊。<img src='events/worldChamp_men.jpg' height='120px' style='float: right;'>";
+$('#1949_t').append(t_1949);
+
+var t_1952 = "<h1>1952s 女子世界錦標賽：</h1>&nbsp;&nbsp;&nbsp;&nbsp;國際排聯舉辦的女子國際賽事，每四年舉辦一次，首屆舉辦於<mark class='yellow'>前蘇聯莫斯科</mark>。此時，排球運動已擴展至亞洲各國。<br><img src='events/worldChamp_women.jpg' height='120px' style='float: right;'>";
+$('#1952_t').append(t_1952);
+
+var t_1964 = "<h1>1964s 進入奧運：</h1>&nbsp;&nbsp;&nbsp;&nbsp;Volleyball 正式成為<mark class='yellow'>夏季奧運</mark>的比賽項目，為使世錦賽和奧運交替進行，1962s世錦賽提早兩年開賽。<br><img src='events/olympic_volley.jpeg' height='130px' style='float: right;'>";
+$('#1964_t').append(t_1964);
+
+var t_1965 = "<h1>1965s 男子世界盃：</h1>&nbsp;&nbsp;&nbsp;&nbsp;國際排聯舉辦的<mark class='yellow'>三大世界賽事之一</mark>，單循環制，12隊參賽隊伍。賽事時間在奧運前一年，<mark class='yellow'>曾為奧運資格賽</mark>之一。目前俄羅斯男排奪冠最多次(6次)。<img src='events/worldcup_men.jpg' height='120px' style='float: right;'>";
+$('#1965_t').append(t_1965);
+
+var t_1970 = "<h1>1970s 非洲加入國際賽事：</h1>&nbsp;&nbsp;&nbsp;&nbsp;非洲是最後一個加入國際賽的區域，此時基本上達成FIVB最初目標：五個洲份聯盟的成員皆加入國際賽事。<br>&nbsp;&nbsp;&nbsp;&nbsp;非洲職業排球聯盟=><a href='https://web.archive.org/web/20131113131017/http://cavb.org/' target='_blank'><img src='events/africa_volleyy.png' height='120px' style='float: right;' title='click me'></a>";
+$('#1970_t').append(t_1970);
+
+var t_1973 = "<h1>1973s 女子世界盃：</h1>&nbsp;&nbsp;&nbsp;&nbsp;國際排聯舉辦的<mark class='yellow'>三大世界賽事之一</mark>，單循環制，12隊參賽隊伍。賽事時間在奧運前一年，<mark class='yellow'>曾為奧運資格賽</mark>之一。1977年時，<mark class='yellow'>日本買斷主辦權</mark>。<img src='events/worldcup_women.jpg' height='120px' style='float: right;'>";
+$('#1973_t').append(t_1973);
+
+var t_1977 = "<h1>1977s U21男子世界杯：</h1>&nbsp;&nbsp;&nbsp;&nbsp;國際排聯舉辦的<mark class='yellow'>青年男子(20以下)</mark>國際賽事，第一屆辦在<mark class='yellow'>巴西里約熱內盧</mark>，本為4年一次，現改為2年一次。奪冠最多為俄羅斯(10次)。";
+$('#1977_t').append(t_1977);
+
+var t_1983 = "<h1>1983s AVP男子職排：</h1>&nbsp;&nbsp;&nbsp;&nbsp;<mark class='yellow'>第一個利益型態的排球組織</mark>，總部位於<mark class='yellow'>美國加利福尼亞州奧蘭治縣新港灘(紐波特比奇)</mark>，舉辦了歷史最長久的沙排巡迴賽，中間曾因破產暫停比賽。<a href='https://avp.com/' target='_blank'><img src='events/AVP.jpg' height='120px' style='float: right;'>";
+$('#1983_t').append(t_1983);
+
+var t_1986 = "<h1>1986s WPAV女子職排：</h1>&nbsp;&nbsp;&nbsp;&nbsp;第一個<mark class='yellow'>女子沙排職業聯盟</mark>，隔年在美國加利福尼亞州奧蘭治縣新港灘(紐波特比奇)舉辦第一場女子比賽。<mark class='yellow'>女子首次主控運動賽事</mark>。";
+$('#1986_t').append(t_1986);
+
+var t_1989 = "<h1>1989s 世界男排俱樂部錦標賽：</h1>&nbsp;&nbsp;&nbsp;&nbsp;由國際排聯舉辦，首屆賽事在<mark class='yellow'>義大利</mark>舉行。現有8隊，來自<mark class='yellow'>各州俱樂部錦標賽冠軍</mark>、<mark class='yellow'>主辦</mark>以及<mark class='yellow'>外卡獲邀球隊</mark>。現以義大利俱樂部奪冠最多(10次)。";
+$('#1989_t').append(t_1989);
+
+var t_1990 = "<h1>1990s 世界聯賽：</h1>&nbsp;&nbsp;&nbsp;&nbsp;由國際排聯舉辦，首屆賽事位於<mark class='yellow'>日本大阪</mark>，為每年一度國際男子排球比賽，是目前最大型及具規模的國際性賽事之一。因舉辦成功，三年後舉辦了<mark class='yellow'>世界女排大獎賽</mark>，性質與世界聯賽相同。";
+$('#1990_t').append(t_1990);
+
+var t_1991 = "<h1>1991s 世界女排俱樂部錦標賽：</h1>&nbsp;&nbsp;&nbsp;&nbsp;由國際排聯舉辦，首屆賽事在<mark class='yellow'>巴西</mark>舉行。目前奪冠最多次的球隊為<mark class='yellow'>土耳其的瓦基弗銀行</mark>。<br><a href='https://www.vakifbanksporkulubu.com/tr/anasayfa' target='_blank'><img src='events/vakifbank.png' height='100px' style='float: right;'></a>";
+$('#1991_t').append(t_1991);
+
+var t_1993 = "<h1>1993s 世界大冠軍杯：</h1>&nbsp;&nbsp;&nbsp;&nbsp;性質與洲際冠軍杯類似，於<mark class='yellow'>奧運後1年</mark>舉行，主辦國固定為<mark class='yellow'>日本</mark>。有時與奧運、世錦賽、世界杯合稱<mark class='yellow'>'排球四大賽事'</mark>，但大冠軍杯<mark class='yellow'>沒有計入國際排聯世界排名積分</mark>。";
+$('#1993_t').append(t_1993);
+
+var t_1998 = "<h1>1998s 落地得分制源起：</h1>&nbsp;&nbsp;&nbsp;&nbsp;<mark class='yellow'>落地得分制(rally point system)</mark>在1988年被提出，1999年開始陸續實施。主要是更能夠<mark class='yellow'>預測比賽時間</mark>，使得比賽過程更為刺激，增加可看性。";
+$('#1998_t').append(t_1998);
+
+var t_2000 = "<h1>2000s 正式改為落地得分制：</h1>&nbsp;&nbsp;&nbsp;&nbsp;最後一場使用'發球得分制'的比賽為<mark class='yellow'>2000s的NCAA I級女排錦標賽</mark>，之後所有比賽正式改為落地得分制。";
+$('#2000_t').append(t_2000);
+
+var t_2002 = "<h1>2002s 世錦賽參賽隊伍增加：</h1>&nbsp;&nbsp;&nbsp;&nbsp;世錦賽為排球奧運之後第二重要的國際賽事，隨著排球的普及，世錦賽由最初的10隊逐漸增加。在2002年，最終定在24隊。";
+$('#2002_t').append(t_2002);
+
+var t_2018 = "<h1>2018s 國家聯賽：</h1>&nbsp;&nbsp;&nbsp;&nbsp;<mark class='yellow'>國家男排聯賽</mark>取代世界排球聯賽，首屆在<mark class='yellow'>法國里耳</mark>。<mark class='yellow'>國家女排聯賽</mark>取代世界女排大獎賽，首屆在<mark class='yellow'>中國南京</mark>。以一組進行比賽，12隊為以往第一組別隊伍，餘下4隊須透過<mark class='yellow'>國家挑戰杯</mark>產生。";
+$('#2018_t').append(t_2018);
+
